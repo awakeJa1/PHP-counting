1bf16fc946c95c3934961c14b2ead3cca7017fc8
João ielen <99980161+awakeJa1@users.noreply.github.com>
2022-10-27 23:42:16 -0300

    Add files via upload

---
+++ contar.php
@@ -0,0 +1,14 @@
+<?php
+
+$n1=$_POST['um'];
+$n2=$_POST['dois'];
+
+
+for($n1;$n1<=$n2;$n1+=2){
+    echo $n1 . " ";
+}
+?>
+
+<br/>
+
+<a href="index.php">Voltar</a>
---
+++ index.php
@@ -0,0 +1,7 @@
+<form action="contar.php" method="POST">
+
+    <input type="number" name="um"/>
+    <input type="number" name="dois"/>
+    <input type="submit" value="Contar"/>
+
+</form>
